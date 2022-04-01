773b5e7b32616b01130242e4e54799abbb1b0dff
Iury Brasileiro <Iury.brasileiro@outlook.com.br>
2022-04-01 19:36:11 -0300

    chore(ios): add keyboard extension

---
+++ ios/keyboard/KeyboardViewController.h
@@ -0,0 +1,12 @@
+//
+//  KeyboardViewController.h
+//  keyboard
+//
+//  Created by Iury Brasileiro on 01/04/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KeyboardViewController : UIInputViewController
+
+@end
